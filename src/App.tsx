d3e84ad048afb6fad71284abd7d379c753a1a9d5
Alex Artemov <146786356+alexartemov7@users.noreply.github.com>
2024-09-21 03:38:57 -0400

    added signup and login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Layout } from './components/Layout'
 import { Home } from './pages/Home'
+import DonorSignup from './pages/DonorForm'
+import SeekerSignup from './pages/SeekerForm'
+import UserLogin from './pages/UserLogin'
 
 function App() {
   return (
@@ -9,6 +12,13 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/donor-signup" element={<DonorSignup />} />
+          <Route path="/user-login" element={<UserLogin />} />
+          {/* <Route path="/donor-dashboard" element={<DonorSignup />} /> */}
+          {/* <Route path="/donor-profile" element={<SeekerProfile />} /> */}
+          <Route path="/seeker-signup" element={<SeekerSignup />} />
+          {/* <Route path="/seeker-dashboard" element={<SeekerSignup />} /> */}
+          {/* <Route path="/seeker-profile" element={<SeekerProfile />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>

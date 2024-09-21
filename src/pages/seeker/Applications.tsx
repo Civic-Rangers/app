ec1649f6d8e4699d23a2c0acd8eddebbfb1a4d83
ken862734801 <kenshawmichaud@gmail.com>
2024-09-21 14:24:53 -0400

    change main color to blue to match other pages

--- src/pages/seeker/Applications.tsx
+++ src/pages/seeker/Applications.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import thumbnailImage from "../../assets/car.png";
 
 const applications = [
-  { id: 1, name: 'John Doe', status: 'Pending' },
-  { id: 2, name: 'Jane Smith', status: 'Pending' },
-  { id: 3, name: 'Alice Johnson', status: 'Pending' },
-  { id: 4, name: 'Chris Lee', status: 'Pending' },
-  { id: 5, name: 'Patricia Brown', status: 'Pending' },
+  { id: 1, name: 'John Doe', image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp', address: '123 Main St, Fort Lauderdale, FL, 33301', thumbnail:thumbnailImage, time:'10:00 AM', status: 'Success' },
+  { id: 2, name: 'Jane Smith', image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp', address: '123 Main St, Fort Lauderdale, FL, 33301', thumbnail:thumbnailImage, time:'10:00 AM', status: 'Failure' },
+  { id: 3, name: 'Alice Johnson', image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp', address: '123 Main St, Fort Lauderdale, FL, 33301', thumbnail:thumbnailImage, time:'10:50 AM', status: 'Pending' },
+  { id: 4, name: 'Chris Lee', image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp', address: '123 Main St, Fort Lauderdale, FL, 33301', thumbnail:thumbnailImage, time:'11:00 AM', status: 'Pending' },
+  { id: 5, name: 'Patricia Brown', image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp', address: '123 Main St, Fort Lauderdale, FL, 33301', thumbnail:thumbnailImage, time:'12:00 PM', status: 'Pending' },
 ]
 
 const Applications: React.FC = () => {
@@ -16,12 +17,38 @@ const Applications: React.FC = () => {
       </header>
       
       <div className="space-y-4">
-        {applications.map(application => (
-          <div key={application.id} className="rounded-lg bg-gray-800 p-4 shadow-md">
-            <h2 className="text-xl font-semibold">{application.name}</h2>
-            <p className="text-gray-400">Status: {application.status}</p>
-          </div>
-        ))}
+      <table className='table-auto w-full mt-4 rounded-lg shadow-xl overflow-hidden'>
+            <thead className='text-white'>
+              <tr className="bg-gray-800 h-12">
+                <th className='p-2 text-left'>Name</th>
+                <th className='p-2 text-left'>Address</th>
+                {/* <th className='p-2 text-left'>Image</th> */}
+                <th className='p-2 text-left'>Time</th>
+                <th className='p-2 text-left'>Status</th>
+              </tr>
+            </thead>
+            <tbody className='text-white bg-gray-800'>
+              {applications.map(application => (
+                <tr key={application.id}>
+                  <td className='p-2 md:flex items-center gap-4'>
+                    <img src={application.image} alt={application.image} className='w-10 h-10 rounded hidden md:block'/>
+                    <div>
+                      <p>{application.name}</p>
+                    </div>
+                </td>
+                  <td>{application.address}</td>
+                  {/* Image looks strange in table. Maybe revisit. */}
+                  {/* <td>
+                    <img src={thumbnailImage} className='w-10 h-10'/>
+                  </td> */}
+                  <td>{application.time}</td>
+                  <td className='p-2'>
+                    <div className={`text-white badge ${application.status === 'Success' ? 'badge-success' : application.status === "Failure" ? "badge-error" : "badge-warning"}`}>{application.status}</div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            </table>
       </div>
     </div>
   )

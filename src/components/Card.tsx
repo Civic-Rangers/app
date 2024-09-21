729dcff09f6c15ae00f40aa0bc32276576c675fe
ken862734801 <kenshawmichaud@gmail.com>
2024-09-21 05:46:16 -0400

    update props to match api data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,19 +4,20 @@ type CardProps = {
     address_street: string;
     address_city: string;
     address_state: string;
+    address_zip?: string
     distance: string;
     available: string;
     imageSrc: string;
     onClick?: () => void;
 };
 
-export function Card({ address_street, address_city, address_state, distance, available, imageSrc, onClick }: CardProps) {
+export function Card({ address_street, address_city, address_state, address_zip, distance, available, imageSrc, onClick }: CardProps) {
     return (
         <div className="card w-96 shadow-xl bg-white text-black m-2" onClick={onClick}>
             <div className="p-5 flex justify-between">
                 <div>
                     <h6 className="font-bold" title={address_street}>{address_street}</h6>
-                    <p className="text-gray-400" title={`${address_city}, ${address_state}`}>{`${address_city}, ${address_state}`}</p>
+                    <p className="text-gray-400" title={`${address_city}, ${address_state}, ${address_zip}`}>{`${address_city}, ${address_state}, ${address_zip}`}</p>
                     <div className="mt-2 flex gap-4">
                         <div className="flex items-center gap-2">
                             <FaCarAlt size={16} />

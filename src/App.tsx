cae518cd0460ce75b9a08024f35f40097b381bc8
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-09-20 23:38:47 -0400

    chore: add prettier and plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,21 +18,16 @@ function App() {
       </div>
       <h1 className="text-red-600">Vite + React</h1>
       <div className="card">
-        <button
-          className="btn btn-warning"
-          onClick={() => setCount((count) => count + 1)}
-        >
+        <button className="btn btn-warning" onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
